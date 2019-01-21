98d7f4e74f4c86c391a8d3e9938b8538596e10ee
HC <rahc@ges.com>
2019-01-21 03:05:24 -0800

    'LoginRouting'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,27 +11,19 @@ import { MainComponent } from './main/main.component';
 import { IntegrationGroupComponent } from './integration-group/integration-group.component';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker'
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 
-=======
-import { LoginComponent } from './login/login.component';
->>>>>>> a401c907648eb1c71fe09bbaf1f9591e3c56aa06
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-<<<<<<< HEAD
     MainComponent,
     IntegrationGroupComponent,
     LoginComponent,
-=======
-    LoginComponent
->>>>>>> a401c907648eb1c71fe09bbaf1f9591e3c56aa06
   ],
   imports: [
     BrowserModule,
